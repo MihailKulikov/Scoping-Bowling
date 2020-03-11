0f9c71cec1c25ff011b5d8e8903a3f9fd9d90623
MihailKulikov <kulikovmihail90@gmail.com>
2020-03-11 10:23:41 +0300

    0.1

---
+++ CrowlingBlowing/CrowlingBlowing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrowlingBlowing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
